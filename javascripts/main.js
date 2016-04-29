d75383f962332ce2734b0c5b95c17ba9abedaf35
Jerome Tolentino <romedtino@gmail.com>
2016-04-29 15:11:14 +0000

    Revert "Keep changing my mind on nav v. buttons"
    This reverts commit d99e46462249ca1110706b4e04ddb3df73c49304.

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,10 +2,8 @@ console.log('main.js loaded.');
 
 $(document).ready(function() {
 
-    $('a').click(function(event) {
+    $('button').click(function(event) {
 
-        event.preventDefault();
-        
         //TODO maybe we want to make sure these are the top three buttons?
         $(document.getElementById("mainportion")).scrollTo(document.getElementById($(this).context.title), 800);
 
