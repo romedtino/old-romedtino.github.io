d99e46462249ca1110706b4e04ddb3df73c49304
Jerome Tolentino <romedtino@gmail.com>
2016-04-29 06:28:34 +0000

    Keep changing my mind on nav v. buttons

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,8 +2,10 @@ console.log('main.js loaded.');
 
 $(document).ready(function() {
 
-    $('button').click(function(event) {
+    $('a').click(function(event) {
 
+        event.preventDefault();
+        
         //TODO maybe we want to make sure these are the top three buttons?
         $(document.getElementById("mainportion")).scrollTo(document.getElementById($(this).context.title), 800);
 
