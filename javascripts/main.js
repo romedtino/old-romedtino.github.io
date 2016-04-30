f8c127b82fef00f5b86d085ff5b1362723f35b33
Jerome Tolentino <romedtino@gmail.com>
2016-04-30 22:00:16 +0000

    Added animations, moved stuff around.

--- javascripts/main.js
+++ javascripts/main.js
@@ -10,4 +10,40 @@ $(document).ready(function() {
 
     });
 
+    $('#mainportion').scroll(function() {
+        
+        checkElementForAnim($("#pictureAndName"));
+        checkElementForAnim($("#Projects"));
+        checkElementForAnim($("#About"));
+        
+    })
+
+    function checkElementForAnim(elem) {
+        var scrolled = isScrolledIntoView($(elem));
+
+        if (scrolled) {
+            if (!$(elem).hasClass("fadeinAnimation")) {
+
+                $(elem).addClass("fadeinAnimation");
+            }
+        }
+        else {
+            $(elem).removeClass("fadeinAnimation");
+
+        }
+    }
+
+    function isScrolledIntoView(elem) {
+        var $elem = $(elem);
+        var $window = $(window);
+
+        var docViewTop = $window.scrollTop();
+        var docViewBottom = docViewTop + $window.height();
+
+        var elemTop = $elem.offset().top;
+        var elemBottom = elemTop + $elem.height();
+
+        return (((elemTop >= docViewTop) && (elemTop <= docViewBottom)) || ((elemBottom >= docViewTop) && (elemBottom <= docViewBottom)))
+    }
+
 });
